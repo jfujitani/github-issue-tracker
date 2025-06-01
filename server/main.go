c3043f07d5d1f1e52637977381badfdcd71d040b
John Fujitani <jfuji6@gmail.com>
2025-05-31 19:32:22 -0700

    handle cors and trailing /

--- server/main.go
+++ server/main.go
@@ -8,24 +8,53 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	server := buildServer()
 
-	r := http.NewServeMux()
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir("./public")))
 
 	// TODO "/api" should be configured through openApi.yaml
 	// h := api.HandlerFromMux(server, r)
-	h := api.HandlerFromMuxWithBaseURL(server, r, "/api")
+	api.HandlerFromMuxWithBaseURL(server, mux, "/api")
+
+	handler := withCORS(stripTrailingSlash(mux))
 
 	s := &http.Server{
-		Handler: h,
+		Handler: handler,
 		Addr:    "0.0.0.0:8080",
 	}
 
 	fmt.Println("Starting server on :8080...")
 	log.Fatal(s.ListenAndServe())
+}
+
+func stripTrailingSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" && strings.HasSuffix(r.URL.Path, "/") {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func withCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func buildServer() *api.Server {
