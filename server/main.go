0b1b669714b83e6537b16707448736b47734b80a
John Fujitani <jfuji6@gmail.com>
2025-06-01 00:12:02 -0700

    don't need to add cors on same origin

--- server/main.go
+++ server/main.go
@@ -22,7 +22,7 @@ func main() {
 	// h := api.HandlerFromMux(server, r)
 	api.HandlerFromMuxWithBaseURL(server, mux, "/api")
 
-	handler := withCORS(stripTrailingSlash(mux))
+	handler := stripTrailingSlash(mux)
 
 	s := &http.Server{
 		Handler: handler,
@@ -38,21 +38,6 @@ func stripTrailingSlash(next http.Handler) http.Handler {
 		if r.URL.Path != "/" && strings.HasSuffix(r.URL.Path, "/") {
 			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
 		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-func withCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-
 		next.ServeHTTP(w, r)
 	})
 }
